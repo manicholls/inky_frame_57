8b400399cfd52ba29b68d2749dae1446d8324e71
Mark Nicholls <mark@nichollsclan.ca>
2026-08-18 18:36:58 -0400

    Refactor display initialization and setup process

--- components/inky_frame_57/inky_frame_57.h
+++ components/inky_frame_57/inky_frame_57.h
@@ -2,24 +2,26 @@
 
 #include "esphome.h"
 #include "esphome/components/spi/spi.h"
+#include "esphome/core/log.h"
 #include <Arduino.h>
 
 namespace esphome {
 namespace inky_frame_57 {
+
+static const char *const TAG = "inky_frame_57";
 
 class InkyFrame57 : public display::DisplayBuffer,
                     public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW,
                                           spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_10MHZ> {
  protected:
   uint8_t *buffer_;
+  bool initialised_ = false;
 
   const int DC_PIN = 28;
   const int RST_PIN = 27;
   const int SR_CLK_PIN = 8;
   const int SR_LATCH_PIN = 9;
   const int SR_DATA_PIN = 10;
-  
-  // Critical Pimoroni Power Pin
   const int HOLD_VSYS_EN_PIN = 2; 
 
   void command(uint8_t command) {
@@ -58,17 +60,58 @@ class InkyFrame57 : public display::DisplayBuffer,
   void wait_busy() {
     while (is_busy()) {
       delay(50);
-      App.feed_wdt(); // Tells ESPHome's watchdog that the system hasn't frozen
+      App.feed_wdt(); 
       yield(); 
     }
   }
 
+  void init_display() {
+    ESP_LOGI(TAG, "Starting Hardware Reset...");
+    digitalWrite(RST_PIN, LOW);
+    delay(20);
+    digitalWrite(RST_PIN, HIGH);
+    delay(20);
+
+    ESP_LOGI(TAG, "Waiting for initial busy state to clear...");
+    wait_busy();
+
+    ESP_LOGI(TAG, "Sending initialization sequence...");
+    command(0x01); 
+    data(0x37); data(0x00); data(0x23); data(0x23);
+
+    command(0x00); 
+    data(0xEF); data(0x08);
+
+    command(0x03); data(0x00);
+
+    command(0x06); 
+    data(0xC7); data(0xC7); data(0x1D);
+
+    command(0x30); data(0x3C);
+
+    command(0x41); data(0x00);
+
+    command(0x50); data(0x37);
+
+    command(0x60); data(0x22);
+
+    command(0x61); 
+    data(0x02); data(0x58); data(0x01); data(0xC0);
+
+    command(0xE3); data(0xAA);
+
+    ESP_LOGI(TAG, "Powering ON E-Ink Panel...");
+    command(0x04); 
+    wait_busy();
+    
+    initialised_ = true;
+    ESP_LOGI(TAG, "Display Initialization Complete!");
+  }
+
  public:
   void setup() override {
-    // 1. TURN ON SYSTEM POWER IMMEDIATELY
     pinMode(HOLD_VSYS_EN_PIN, OUTPUT);
     digitalWrite(HOLD_VSYS_EN_PIN, HIGH);
-    delay(50); // Give the peripherals a moment to wake up
       
     pinMode(DC_PIN, OUTPUT);
     pinMode(RST_PIN, OUTPUT);
@@ -81,67 +124,33 @@ class InkyFrame57 : public display::DisplayBuffer,
     buffer_ = new uint8_t[600 * 448 / 2];
     memset(buffer_, 0x11, 600 * 448 / 2); 
 
-    digitalWrite(RST_PIN, LOW);
-    delay(20);
-    digitalWrite(RST_PIN, HIGH);
-    delay(20);
-
-    wait_busy();
-
-    command(0x01); 
-    data(0x37);
-    data(0x00);
-    data(0x23);
-    data(0x23);
-
-    command(0x00); 
-    data(0xEF);
-    data(0x08);
-
-    command(0x03); 
-    data(0x00);
-
-    command(0x06); 
-    data(0xC7);
-    data(0xC7);
-    data(0x1D);
-
-    command(0x30); 
-    data(0x3C);
-
-    command(0x41); 
-    data(0x00);
-
-    command(0x50); 
-    data(0x37);
-
-    command(0x60); 
-    data(0x22);
-
-    command(0x61); 
-    data(0x02);
-    data(0x58);
-    data(0x01);
-    data(0xC0);
-
-    command(0xE3); 
-    data(0xAA);
-
-    command(0x04); 
-    wait_busy();
+    // Defer the heavy initialization by 3 seconds to let USB and WiFi connect
+    this->set_timeout(3000, [this]() {
+        this->init_display();
+        this->update(); // Trigger the first drawing automatically
+    });
   }
 
   void update() override {
+    if (!initialised_) {
+      ESP_LOGW(TAG, "Display not initialised yet, skipping update.");
+      return;
+    }
+
+    ESP_LOGI(TAG, "Rendering ESPHome graphics...");
     this->do_update_();
     
+    ESP_LOGI(TAG, "Transmitting buffer to display...");
     command(0x10); 
     digitalWrite(DC_PIN, HIGH);
     this->enable();
     this->write_array(buffer_, 600 * 448 / 2);
     this->disable();
     
+    ESP_LOGI(TAG, "Commanding screen refresh (this takes up to 30s)...");
     command(0x12); 
     wait_busy();
+    ESP_LOGI(TAG, "Screen refresh complete.");
   }
 
   void draw_absolute_pixel_internal(int x, int y, Color color) override {
