c7e0f26022f461406117422b82913d7dfa6bbf76
Mark Nicholls <mark@nichollsclan.ca>
2026-08-18 17:34:03 -0400

    Refactor display registration in InkyFrame57
    Updated display registration to automatically include component registration.

--- components/inky_frame_57/display.py
+++ components/inky_frame_57/display.py
@@ -10,7 +10,6 @@ InkyFrame57 = inky_frame_ns.class_(
     "InkyFrame57", cg.PollingComponent, display.DisplayBuffer, spi.SPIDevice
 )
 
-# Inherit standard display and SPI properties (like spi_id and update_interval)
 CONFIG_SCHEMA = display.FULL_DISPLAY_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(InkyFrame57),
 }).extend(cv.polling_component_schema("60s")).extend(spi.spi_device_schema())
@@ -20,6 +19,7 @@ async def to_code(config):
     cg.add_global(cg.RawStatement('#include "inky_frame_57.h"'))
     
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+    
+    # Registering the display automatically registers the component
     await display.register_display(var, config)
     await spi.register_spi_device(var, config)
