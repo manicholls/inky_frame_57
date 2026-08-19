56793d36087ce2771879c7a64f6dbfa0e66d2450
Mark Nicholls <mark@nichollsclan.ca>
2026-08-18 21:07:56 -0400

    Refactor InkyFrame57 class and update CONFIG_SCHEMA

--- components/inky_frame_57/display.py
+++ components/inky_frame_57/display.py
@@ -7,18 +7,16 @@ DEPENDENCIES = ["spi"]
 
 inky_frame_ns = cg.esphome_ns.namespace("inky_frame_57")
 InkyFrame57 = inky_frame_ns.class_(
-    "InkyFrame57", cg.PollingComponent, display.DisplayBuffer, spi.SPIDevice
+    "InkyFrame57", display.DisplayBuffer, spi.SPIDevice
 )
 
+# Set cs_pin_required to False so we can manage it manually in C++
 CONFIG_SCHEMA = display.FULL_DISPLAY_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(InkyFrame57),
-}).extend(cv.polling_component_schema("60s")).extend(spi.spi_device_schema())
+}).extend(cv.polling_component_schema("60s")).extend(spi.spi_device_schema(cs_pin_required=False))
 
 async def to_code(config):
-    # Specify the exact internal path where ESPHome places external component files
     cg.add_global(cg.RawStatement('#include "esphome/components/inky_frame_57/inky_frame_57.h"'))
-    
     var = cg.new_Pvariable(config[CONF_ID])
-    
     await display.register_display(var, config)
     await spi.register_spi_device(var, config)
