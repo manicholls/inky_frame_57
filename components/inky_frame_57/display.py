c9f27e472e776298ac33357742152f4f95f07bb1
Mark Nicholls <mark@nichollsclan.ca>
2026-08-20 11:45:40 -0400

    Fix string quotes and update display configuration

--- components/inky_frame_57/display.py
+++ components/inky_frame_57/display.py
@@ -3,20 +3,17 @@ import esphome.config_validation as cv
 from esphome.components import display, spi
 from esphome.const import CONF_ID
 
-DEPENDENCIES = ["spi"]
+DEPENDENCIES = ['spi']
 
-inky_frame_ns = cg.esphome_ns.namespace("inky_frame_57")
-InkyFrame57 = inky_frame_ns.class_(
-    "InkyFrame57", display.DisplayBuffer, spi.SPIDevice
-)
+inky_frame_57_ns = cg.esphome_ns.namespace('inky_frame_57')
+InkyFrame57 = inky_frame_57_ns.class_('InkyFrame57', display.DisplayBuffer, spi.SPIDevice)
 
-# Set cs_pin_required to False so we can manage it manually in C++
 CONFIG_SCHEMA = display.FULL_DISPLAY_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(InkyFrame57),
-}).extend(cv.polling_component_schema("60s")).extend(spi.spi_device_schema(cs_pin_required=False))
+}).extend(spi.spi_device_schema())
 
 async def to_code(config):
-    cg.add_global(cg.RawStatement('#include "esphome/components/inky_frame_57/inky_frame_57.h"'))
     var = cg.new_Pvariable(config[CONF_ID])
+    # THIS is the missing magic line that compiles your YAML lambda into C++!
     await display.register_display(var, config)
     await spi.register_spi_device(var, config)
