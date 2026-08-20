5af0fb284e54008720edadc9d33b0d5ac18e931b
Mark Nicholls <mark@nichollsclan.ca>
2026-08-20 12:38:44 -0400

    Implement lambda compilation for display buffer
    Added support for CONF_LAMBDA to compile YAML lambda into C++ function for display buffer.

--- components/inky_frame_57/display.py
+++ components/inky_frame_57/display.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import display, spi
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_LAMBDA
 
 DEPENDENCIES = ['spi']
 
@@ -14,6 +14,19 @@ CONFIG_SCHEMA = display.FULL_DISPLAY_SCHEMA.extend({
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    # THIS is the missing magic line that compiles your YAML lambda into C++!
+    
+    # 1. Register the display and SPI device natively
     await display.register_display(var, config)
     await spi.register_spi_device(var, config)
+    
+    # 2. THE MISSING LINK: FORCE LAMBDA COMPILATION
+    # This explicitly translates your YAML lambda into a C++ function 
+    # and attaches it to the display buffer's graphics engine.
+    if CONF_LAMBDA in config:
+        display_ns = cg.esphome_ns.namespace('display')
+        DisplayBufferRef = display_ns.class_('DisplayBuffer').operator('ptr')
+        
+        lambda_ = await cg.process_lambda(
+            config[CONF_LAMBDA], [(DisplayBufferRef, "it")], return_type=cg.void
+        )
+        cg.add(var.set_writer(lambda_))
